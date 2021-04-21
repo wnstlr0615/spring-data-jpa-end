1c29736d9758572894ad4dcdc0bc14bef5d1b3ef
wnstlr0615 <ryan0@kakao.com>
2021-04-21 18:36:14 +0900

    spring data jpa 끝

--- src/main/java/study/querydsl/querydsl/controller/MemberController.java
+++ src/main/java/study/querydsl/querydsl/controller/MemberController.java
@@ -2,6 +2,9 @@ package study.querydsl.querydsl.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +21,11 @@ public class MemberController {
     private final MemberRepository memberRepository;
     @PostConstruct
     public void init(){
-        memberRepository.save(new Member("jjon", 14));
+        memberRepository.save(new Member("member1", 14));
+        memberRepository.save(new Member("member1", 24));
+        memberRepository.save(new Member("member3", 34));
+        memberRepository.save(new Member("member4", 44));
+        memberRepository.save(new Member("member5", 54));
     }
     @GetMapping("/members/v1/{id}")
     public MemberDto findMember(@PathVariable("id") Long id){
@@ -28,5 +35,9 @@ public class MemberController {
     @GetMapping("/members/v2/{id}")
     public MemberDto findMember(@PathVariable("id") Member member){
         return new MemberDto(member.getUsername(), member.getAge());
+    }
+    @GetMapping("/members")
+    public Page<Member> list(@PageableDefault() Pageable pageable){
+        return memberRepository.findAll(pageable);
     }
 }
---
+++ src/main/java/study/querydsl/querydsl/entity/Item.java
@@ -0,0 +1,25 @@
+package study.querydsl.querydsl.entity;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Persistable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Item extends BaseTimeEntity implements Persistable<String> {
+    @Id
+    private String id;
+
+    @Override
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public boolean isNew() {
+        return getCreateDate()==null;
+    }
+}
